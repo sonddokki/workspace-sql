60776faade3fcb3c48e6c7661f0a2dc6aff2d2df
sonddokki <sonsharo@naver.com>
2023-09-01 12:41:01 +0900

    0901 미니프로젝트 테이블관리

--- webdb 테이블관리/person 테이블관리.sql
+++ webdb 테이블관리/person 테이블관리.sql
@@ -26,7 +26,25 @@ nocache;                       -- 번호미리생성 안하기
 
 
 INSERT INTO person
-VALUES (seq_person_id.nextval, '손성진' , '010-7130-9913' , '하이미디어'  );
+VALUES (seq_person_id.nextval, '손성진' , '010-7130-9913' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '성진손' , '111-2222-3334' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '이정재' , '222-3333-4444' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '손바닥' , '010-7130-9913' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '손만두' , '010-7130-9913' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '침착맨' , '555-5555-3333' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '풍월량' , '111-5455-2333' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '우왁굳' , '421-3232-2322' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '소니쇼' , '010-7130-9913' , '412-2312-2312'  );
+INSERT INTO person
+VALUES (seq_person_id.nextval, '정우성' , '010-7130-9913' , '412-2312-2312'  );
 
 
 DELETE FROM person 
@@ -38,3 +56,34 @@ SELECT  person_id
         ,company    
 FROM person
 where name like '%성%' ;
+
+-- 테이블 출력 테스트
+
+-- 리스트 아이디 공백주기 확인
+SELECT  lpad(person_id, 2, '0') 
+        ,name 
+        ,hp 
+       ,company 
+FROM person ;
+
+-- 수정
+UPDATE person
+set hp = '000-000-0000'
+where name = '손성진';   
+
+UPDATE person
+set company = '999-999-9999'
+where name = '손성진';   
+
+
+SELECT  person_id
+        ,name 
+        ,hp 
+       ,company 
+FROM person
+where person_id = 1;
+
+
+UPDATE person
+set company = '999-999-9999'
+where name = '손성진';   
